94bfe0f7160dd3e084a9d5429ac68db175e1f216
joaquim <joaquim.mesdesjr@gmail.com>
2023-02-21 20:05:36 -0300

    criação da pagina home

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,52 @@
-import { Container } from "./styles"
+import { Container, Links, Content } from "./styles"
+
+import { Tag } from "../../components/Tag"
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { ButtonText } from "../../components/ButtonText/"
+
 
 export function Details() {
  
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Joaquim Mendes</span>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota"/>
+
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ratione odio veniam tenetur doloremque distinctio?
+            Ducimus est illo rerum aliquid consequatur,
+            corporis numquam cum earum?
+            Magnam omnis sint debitis totam nihil?
+
+
+            
+          </p>
+
+          <Section title="links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+              <li><a href="#">https://www.rocketseat.com.br</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+
+          <Button title= "Voltar" loading/>      
+        </Content>
+      </main>
     </Container>
   )
 }
